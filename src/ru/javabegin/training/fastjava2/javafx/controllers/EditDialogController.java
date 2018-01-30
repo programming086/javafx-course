af4614db1e17bbcfb7c2a87e5057c57d8a20749b
Roman Brovko <brovko-roman@ya.ru>
2018-01-30 21:14:11 +0200

    lesson 14

---
+++ src/ru/javabegin/training/fastjava2/javafx/controllers/EditDialogController.java
@@ -0,0 +1,4 @@
+package ru.javabegin.training.fastjava2.javafx.controllers;
+
+public class EditDialogController {
+}
--- src/ru/javabegin/training/fastjava2/javafx/controllers/MainController.java
+++ src/ru/javabegin/training/fastjava2/javafx/controllers/MainController.java
@@ -1,4 +1,36 @@
 package ru.javabegin.training.fastjava2.javafx.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MainController {
+
+    public void showDialog(ActionEvent actionEvent) {
+
+        try {
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("../fxml/edit.fxml"));
+            stage.setTitle("Редактирование записи");
+            stage.setMinHeight(150);
+            stage.setMinWidth(300);
+            stage.setResizable(false);
+            stage.setScene(new Scene(root));
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner(((Node)actionEvent.getSource()).getScene().getWindow());
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
 }
