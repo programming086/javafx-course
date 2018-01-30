52a6f97cb3a1a7fe07e6c4f531a55cd1a9929619
Roman Brovko <brovko-roman@ya.ru>
2018-01-30 21:10:23 +0200

    lesson 9

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Scene scene = new Scene(root, 300, 275);
+
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
