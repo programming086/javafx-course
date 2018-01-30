7da90a83def0a90aa9c408228d25464ad345b3c0
Roman Brovko <brovko-roman@ya.ru>
2018-01-30 21:10:58 +0200

    lesson 10

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,15 @@
 package sample;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+
+    public void firstButtonAction(ActionEvent actionEvent) {
+        System.out.println("button1");
+
+    }
+
+    public void secButtonAction(ActionEvent actionEvent) {
+        System.out.println("button2");
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,9 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        Scene scene = new Scene(root, 300, 275);
-
-        primaryStage.setScene(scene);
+        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
