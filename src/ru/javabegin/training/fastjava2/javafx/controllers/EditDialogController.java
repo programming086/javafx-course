2dc32d751efe36cec886dcde05a381bfe1212249
Roman Brovko <brovko-roman@ya.ru>
2018-01-30 21:15:28 +0200

    lesson 15 home work

--- src/ru/javabegin/training/fastjava2/javafx/controllers/EditDialogController.java
+++ src/ru/javabegin/training/fastjava2/javafx/controllers/EditDialogController.java
@@ -1,7 +1,23 @@
 package ru.javabegin.training.fastjava2.javafx.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
 public class EditDialogController {
 
+    @FXML
+    private Button btnOk;
+
+    @FXML
+    private Button btnCancel;
+
+    @FXML
+    private TextField txtFIO;
+
+    @FXML
+    private TextField txtPhone;
 
 
 
--- src/ru/javabegin/training/fastjava2/javafx/controllers/MainController.java
+++ src/ru/javabegin/training/fastjava2/javafx/controllers/MainController.java
@@ -1,29 +1,50 @@
 package ru.javabegin.training.fastjava2.javafx.controllers;
 
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Background;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+
+
 
 public class MainController {
 
     @FXML
-    private Button addButton;
+    private Button btnAdd;
+
+    @FXML
+    private Button btnEdit;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private TextField txtSearch;
+
+    @FXML
+    private Button btnSearch;
+
+    @FXML
+    private TableView tableAddressBook;
+
+    @FXML
+    private Label labelCount;
+
+
+
 
     public void showDialog(ActionEvent actionEvent) {
 
         try {
-            addButton.setText("clicked!");
 
             Stage stage = new Stage();
             Parent root = FXMLLoader.load(getClass().getResource("../fxml/edit.fxml"));
