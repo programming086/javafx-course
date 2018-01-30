de1a4daae56a773691eda66efdc702cce5d37b86
Roman Brovko <brovko-roman@ya.ru>
2018-01-30 21:11:41 +0200

    lesson 12

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,15 +1,4 @@
 package sample;
 
-import javafx.event.ActionEvent;
-
 public class Controller {
-
-    public void firstButtonAction(ActionEvent actionEvent) {
-        System.out.println("button1");
-
-    }
-
-    public void secButtonAction(ActionEvent actionEvent) {
-        System.out.println("button2");
-    }
 }
